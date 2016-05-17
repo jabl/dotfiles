7c7d43041aed0f2a8dd7d5537b4c6a38217bd447
Janne Blomqvist <janne.blomqvist@aalto.fi>
2016-05-17 11:25:29 +0300

    More emacs setup

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,3 +1,4 @@
+;; Setup package repositories
 (require 'package)
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/"))
@@ -5,6 +6,27 @@
   ;; For important compatibility libraries like cl-lib
   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
 (package-initialize)
+
+;; Default dir for extra .el files
+(add-to-list 'load-path (expand-file-name "~/.emacs.d/lisp") t)
+
+;; Misc
+(require 'generic-x)
+(column-number-mode t)
+(setq inhibit-splash-screen t)
+(show-paren-mode t)
+
+;; Various file formats..
+; Markdown-mode (not automatic unless installed via elpa)
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+
+
+;; Programming stuff
+
+;; C
+(setq c-default-style "linux")
+
 
 ;; Rust setup
 (add-hook 'rust-mode-hook 'cargo-minor-mode)
@@ -16,3 +38,13 @@
 (add-hook 'racer-mode-hook #'eldoc-mode)
 (add-hook 'racer-mode-hook #'company-mode)
 (add-hook 'flycheck-mode-hook #'flycheck-rust-setup)
+
+
+;; UTF-8
+;; Is this actually necessary on emacs >=24 anymore?
+(setq locale-coding-system 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-selection-coding-system 'utf-8)
+(prefer-coding-system 'utf-8)
+(set-language-environment 'UTF-8)
