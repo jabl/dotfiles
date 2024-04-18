750ec00136ada805f859d5f31ccd209a94bc28c8
Janne Blomqvist <jblomqvist@nvidia.com>
2024-04-18 08:41:13 +0300

    More barebones emacs config

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -24,13 +24,13 @@
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/lisp") t)
 
 ;; Make sure necessary packages are installed
-(setq my-packages
-      '(paredit))
+;(setq my-packages
+;      '(paredit))
 
 ;; Iterate on packages and install missing ones
-(dolist (pkg my-packages)
-  (unless (package-installed-p pkg)
-    (package-install pkg)))
+;(dolist (pkg my-packages)
+;  (unless (package-installed-p pkg)
+;    (package-install pkg)))
 
 ;; Misc
 (require 'generic-x)
@@ -72,13 +72,13 @@
 ;; Lisp
 
 ;; paredit
-(autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
-(add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
-(add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
-(add-hook 'ielm-mode-hook             #'enable-paredit-mode)
-(add-hook 'lisp-mode-hook             #'enable-paredit-mode)
-(add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
-(add-hook 'scheme-mode-hook           #'enable-paredit-mode)
+;(autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
+;(add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
+;(add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
+;(add-hook 'ielm-mode-hook             #'enable-paredit-mode)
+;(add-hook 'lisp-mode-hook             #'enable-paredit-mode)
+;(add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
+;(add-hook 'scheme-mode-hook           #'enable-paredit-mode)
 
 ;; slime
 ;(add-hook 'slime-repl-mode-hook #'enable-paredit-mode)
